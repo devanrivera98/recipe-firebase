c8bca360a7fb258adaacab1c2ee02398fa33d584
Devan Rivera <devanrivera98@gmail.com>
2024-07-24 22:10:16 +0000

    testing an option to add recipes

--- recipe-project/src/App.tsx
+++ recipe-project/src/App.tsx
@@ -1,19 +1,19 @@
 import { Auth } from './components/Auth';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { db } from './config/firebase-config';
 import { getDocs, collection } from 'firebase/firestore';
+import AddRecipe from './components/Firebase/AddRecipe';
 
-// type Recipe = {
-//   name: string;
-//   prepTime: number;
-//   id: string;
-//   dairyFree: boolean;
-// }
-
+type Recipe = {
+  name: string;
+  prepTime: number;
+  id: string;
+  dairyFree: boolean;
+};
 
 function App() {
-  // const [recipeList, setRecipeList] = useState<Recipe[]>([]);
+  const [recipeList, setRecipeList] = useState<Recipe[]>([]);
   const recipeCollectionRef = collection(db, 'recipes');
 
   useEffect(() => {
@@ -21,12 +21,15 @@ function App() {
       try {
         const data = await getDocs(recipeCollectionRef);
         console.log(data);
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data() ,
-          id: doc.id,
-        }));
-        console.log(filteredData)
-        // setRecipeList(filteredData)
+        const filteredData: Recipe[] = data.docs.map((doc) => {
+          const recipeData = doc.data() as Omit<Recipe, 'id'>;
+          return {
+            ...recipeData,
+            id: doc.id,
+          };
+        });
+        console.log('this is filtered data', filteredData);
+        setRecipeList(filteredData);
       } catch (err) {
         console.error(err);
       }
@@ -38,13 +41,15 @@ function App() {
   return (
     <>
       <Auth />
-      {/* <div>
+      <AddRecipe collection={recipeCollectionRef} />
+      <div>
         {recipeList.map((recipe) => (
-          <div>
+          <div key={recipe.id}>
             <h1>{recipe.name}</h1>
+            <p>{recipe.prepTime}</p>
           </div>
-        )}
-      </div> */}
+        ))}
+      </div>
     </>
   );
 }
