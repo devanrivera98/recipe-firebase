579cc73bfa5ab47add1f1d9c786347061a7baf85
Devan Rivera <devanrivera98@gmail.com>
2024-07-27 00:11:17 +0000

    update name works properly

--- recipe-project/src/components/PersonalRecipes/PersonalRecipeList.tsx
+++ recipe-project/src/components/PersonalRecipes/PersonalRecipeList.tsx
@@ -1,8 +1,12 @@
 import { useRecipeList } from "../../hooks/useRecipeList"
 import { deleteRecipe } from "../../services/deleteRecipe";
+import { updateRecipeTitle } from "../../services/updateRecipeTitle";
+import { useState } from "react";
 
 
 export default function PersonalRecipeList() {
+
+  const [updatedTitle, setUpdatedTitle] = useState("");
 
   const recipeList = useRecipeList();
   console.log("here is all the recipe", recipeList);
@@ -15,6 +19,8 @@ export default function PersonalRecipeList() {
             <h1>{recipe.name}</h1>
             <p>{recipe.prepTime}</p>
             <button onClick={() => deleteRecipe(recipe.id)}>Delete Movie</button>
+            <input placeholder="new title..." onChange={(e) => setUpdatedTitle(e.target.value)} />
+            <button onClick={() => updateRecipeTitle(recipe.id, updatedTitle)}>Update Title</button>
           </div>
         ))}
       </div>
