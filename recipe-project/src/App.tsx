fee6f9666db4f82ba2212102ec585d9d3400de30
Devan Rivera <devanrivera98@gmail.com>
2024-08-09 22:59:28 +0000

    removed unused code

--- recipe-project/src/App.tsx
+++ recipe-project/src/App.tsx
@@ -6,30 +6,8 @@ import AddRecipe from './components/PersonalRecipes/AddRecipe';
 import PersonalRecipeList from './components/PersonalRecipes/PersonalRecipeList';
 
 function App() {
-  // const [recipeList, setRecipeList] = useState<Recipe[]>([]);
+  
   const recipeCollectionRef = collection(db, 'recipes');
-
-  // useEffect(() => {
-  //   const getRecipeList = async () => {
-  //     try {
-  //       const data = await getDocs(recipeCollectionRef);
-  //       console.log(data);
-  //       const filteredData: Recipe[] = data.docs.map((doc) => {
-  //         const recipeData = doc.data() as Omit<Recipe, 'id'>;
-  //         return {
-  //           ...recipeData,
-  //           id: doc.id,
-  //         };
-  //       });
-  //       console.log('this is filtered data', filteredData);
-  //       setRecipeList(filteredData);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getRecipeList();
-  // }, []);
 
   return (
     <>
