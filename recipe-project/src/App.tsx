6e3785d9ed3cf8f7a7f745f3c29d68772e0a4a0f
Devan Rivera <devanrivera98@gmail.com>
2024-08-28 22:34:38 +0000

    moved my testing page to demo page

--- recipe-project/src/App.tsx
+++ recipe-project/src/App.tsx
@@ -1,19 +1,11 @@
-import { Auth } from './components/Auth';
 import './App.css';
-import { db } from './config/firebase-config';
-import { collection } from 'firebase/firestore';
-import AddRecipe from './components/PersonalRecipes/AddRecipe';
-import PersonalRecipeList from './components/PersonalRecipes/PersonalRecipeList';
 
 function App() {
 
-  const recipeCollectionRef = collection(db, 'recipes');
 
   return (
     <>
-      <Auth />
-      <AddRecipe collection={recipeCollectionRef} />
-      <PersonalRecipeList />
+
     </>
   );
 }
