87f96004cc1b7a9ff1b1afcef5af993e29618862
Devan Rivera <devanrivera98@gmail.com>
2024-08-29 22:55:41 +0000

    working on the header

--- recipe-project/src/components/Header/Header.tsx
+++ recipe-project/src/components/Header/Header.tsx
@@ -1,8 +1,19 @@
 export default function Header() {
-  
+
   return (
     <>
-
+      <header>
+        <div >
+          <div>
+            <h1>Heirloom</h1>
+          </div>
+          <div>
+            <ul>
+              <li className="text-xl">Log In</li>
+            </ul>
+          </div>
+        </div>
+      </header>
     </>
   )
 }
