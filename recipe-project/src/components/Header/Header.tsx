04185813362790414603a5d3cfd877d4fd20cbfb
Devan Rivera <devanrivera98@gmail.com>
2024-09-02 22:33:55 +0000

    added the desktop layout of the header

--- recipe-project/src/components/Header/Header.tsx
+++ recipe-project/src/components/Header/Header.tsx
@@ -2,14 +2,16 @@ export default function Header() {
 
   return (
     <>
-      <header>
-        <div >
+      <header className="h-20 container mx-auto">
+        <div className="h-full flex justify-between items-center">
           <div>
-            <h1>Heirloom</h1>
+            <h1 className="text-xl">Heirloom</h1>
           </div>
           <div>
-            <ul>
+            <ul className="flex justify-between gap-8">
               <li className="text-xl">Log In</li>
+              <li className="text-xl">Recipes</li>
+              <li className="text-xl">Tips</li>
             </ul>
           </div>
         </div>
