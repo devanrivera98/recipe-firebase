1d44abee6dd6bbee8fa98ecdb9124bf3c8dbe010
Devan Rivera <devanrivera98@gmail.com>
2024-09-17 23:17:58 +0000

    starting to implement main page content

--- recipe-project/src/App.tsx
+++ recipe-project/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import './index.css';
 import Header from './components/Header/Header';
+import HomePage from './pages/Home/HomePage';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <HomePage />
     </>
   );
 }
---
+++ recipe-project/src/pages/Home/HomePage.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="w-full">
+        <img className="w-1/2" alt="martha-stewart-recipe-collection" src="./images/martha-stewart-photo.jpg" />
+        </div>
+      </div>
+    </>
+  )
+}
