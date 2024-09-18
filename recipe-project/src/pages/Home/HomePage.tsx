6278c8883f129078b28c5fa2c47d4da47a214167
Devan Rivera <devanrivera98@gmail.com>
2024-09-18 03:48:26 +0000

    working on setting items in the grid sidebar on homepage

--- recipe-project/src/pages/Home/HomePage.tsx
+++ recipe-project/src/pages/Home/HomePage.tsx
@@ -1,11 +1,26 @@
+import './home.css'
+
 export default function HomePage() {
 
   return (
     <>
       <div className="container mx-auto">
-        <div className="w-full">
-        <img className="w-1/2" alt="martha-stewart-recipe-collection" src="./images/martha-stewart-photo.jpg" />
-        </div>
+        <section className="home-top-of-page">
+          <img alt="martha-stewart-recipe-collection" src="./images/martha-stewart-photo.jpg" />
+          <div className="bg-lightGreen">
+            <h1 className='text-center py-2'>Food News</h1>
+            <div className='grid-cols-1'>
+              <a className='flex'>
+                <div className='w-28 h-20'>
+                  <img className='w-full h-full' src='./images/martha-stewart-photo.jpg' />
+                </div>
+                <div>
+                  <span>Ina Garten's Beef Stew Recipe </span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   )
