da9a51b63608883644e77adfc7bd9230abb86134
Devan Rivera <devanrivera98@gmail.com>
2024-09-19 04:15:25 +0000

    seperated main content to its own component

--- recipe-project/src/pages/Home/HomePage.tsx
+++ recipe-project/src/pages/Home/HomePage.tsx
@@ -1,4 +1,5 @@
 import './home.css'
+import HomeMainContent from './components/HomeMainContent'
 
 export default function HomePage() {
 
@@ -7,46 +8,7 @@ export default function HomePage() {
   return (
     <>
       <div className="container mx-auto">
-        <section className="home-top-of-page pt-28 gap-5">
-          <img alt="martha-stewart-recipe-collection" src="./images/martha-stewart-photo.jpg" />
-          <div className="bg-fadedGreen">
-            <h1 className='text-center py-2'>Food News</h1>
-            <div className='grid grid-cols-1 gap-7 py-2'>
-              <a className='flex mx-5'>
-                <div className='w-28 h-20 flex-shrink-0'>
-                  <img className='w-full h-full' src='./images/martha-stewart-photo.jpg' />
-                </div>
-                <div className='px-2'>
-                  <span>Ina Garten's Beef Stew Recipe Is What We're Making</span>
-                </div>
-              </a>
-              <a className='flex mx-5'>
-                <div className='w-28 h-20 flex-shrink-0'>
-                  <img className='w-full h-full' src='./images/martha-stewart-photo.jpg' />
-                </div>
-                <div className='px-2'>
-                  <span>These Apple Recipes Are Trending, and We Want to Try All of Them</span>
-                </div>
-              </a>
-              <a className='flex mx-5'>
-                <div className='w-28 h-20 flex-shrink-0'>
-                  <img className='w-full h-full' src='./images/martha-stewart-photo.jpg' />
-                </div>
-                <div className='px-2'>
-                  <span>These Apple Recipes Are Trending, and We Want to Try All of Them</span>
-                </div>
-              </a>
-              <a className='flex mx-5'>
-                <div className='w-28 h-20 flex-shrink-0'>
-                  <img className='w-full h-full' src='./images/martha-stewart-photo.jpg' />
-                </div>
-                <div className='px-2'>
-                  <span>These Apple Recipes Are Trending, and We Want to Try All of Them</span>
-                </div>
-              </a>
-            </div>
-          </div>
-        </section>
+        <HomeMainContent />
       </div>
     </>
   )
---
+++ recipe-project/src/pages/Home/components/HomeMainContent.tsx
@@ -0,0 +1,48 @@
+import '../home.css'
+
+export default function HomeMainContent() {
+  return (
+    <>
+      <section className="home-top-of-page pt-16 px-2 gap-5">
+          <img alt="martha-stewart-recipe-collection" src="./images/martha-stewart-photo.jpg" />
+          <div className="bg-fadedGreen">
+            <h1 className='text-center py-2'>Food News</h1>
+            <div className='grid grid-cols-1 gap-7 py-2'>
+              <a className='flex mx-5'>
+                <div className='w-28 h-20 flex-shrink-0'>
+                  <img className='w-full h-full' src='./images/martha-stewart-photo.jpg' />
+                </div>
+                <div className='px-2'>
+                  <span>Ina Garten's Beef Stew Recipe Is What We're Making</span>
+                </div>
+              </a>
+              <a className='flex mx-5'>
+                <div className='w-28 h-20 flex-shrink-0'>
+                  <img className='w-full h-full' src='./images/martha-stewart-photo.jpg' />
+                </div>
+                <div className='px-2'>
+                  <span>These Apple Recipes Are Trending, and We Want to Try All of Them</span>
+                </div>
+              </a>
+              <a className='flex mx-5'>
+                <div className='w-28 h-20 flex-shrink-0'>
+                  <img className='w-full h-full' src='./images/martha-stewart-photo.jpg' />
+                </div>
+                <div className='px-2'>
+                  <span>These Apple Recipes Are Trending, and We Want to Try All of Them</span>
+                </div>
+              </a>
+              <a className='flex mx-5'>
+                <div className='w-28 h-20 flex-shrink-0'>
+                  <img className='w-full h-full' src='./images/martha-stewart-photo.jpg' />
+                </div>
+                <div className='px-2'>
+                  <span>These Apple Recipes Are Trending, and We Want to Try All of Them</span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </section>
+    </>
+  )
+}
