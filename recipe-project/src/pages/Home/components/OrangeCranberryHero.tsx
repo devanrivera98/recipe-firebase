4d3a595a084b9c58f6fac9cf7da443ff869d207a
Devan Rivera <devanrivera98@gmail.com>
2024-09-26 22:13:03 +0000

    made the frame of card for hero

--- recipe-project/src/pages/Home/components/OrangeCranberryHero.tsx
+++ recipe-project/src/pages/Home/components/OrangeCranberryHero.tsx
@@ -6,6 +6,17 @@ export default function OrangeCranberryHero() {
         <div className="orange-cranberry-aspect">
           <img className="w-full h-full" src="./images/orange-cranberry-photo.png"></img>
         </div>
+        <div className="flex flex-col w-96">
+          <div className="p-6">
+            <div>
+            <span className="text-lg">IN THE KITCHEN</span>
+            </div>
+            <span className="text-4xl my-0.5 py-4">Cookin' 9 to 5 With the Parton Sisters</span>
+            <div>
+            <p className="text-lg mb-4">Read more about their new cookbook and learn what "home" taste like to Dolly and Rachel.</p>
+            </div>
+          </div>
+        </div>
       </a>
     </section>
     </>
