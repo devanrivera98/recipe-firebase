992a5a85160ac5ecbece97c4e873da8744ccf4db
Devan Rivera <devanrivera98@gmail.com>
2024-10-03 23:01:57 +0000

    adding images for homepage recipes

--- recipe-project/src/pages/Home/components/QuickEasyRecipes.tsx
+++ recipe-project/src/pages/Home/components/QuickEasyRecipes.tsx
@@ -1,3 +1,5 @@
+import useRecipeId from '../../../hooks/useRecipeId'
+import useSearchApi from '../../../hooks/useSearchApi'
 import './pickedRecipes.css'
 import RecMappedRecipes from './RecMappedRecipes'
 
@@ -7,30 +9,65 @@ export default function QuickEasyRecipes() {
 
   const quickRecipes = [
     {
-      id: 1,
-      image: "./images/apple-photo.jpg",
-      foodType: 'Chicken',
-      name: 'Balsamic Chicken'
+      id: 662917,
+      image: "./images/meatloaf-photo.png",
+      foodType: 'Beef',
+      name: 'Tasty Easy Meatloaf',
+      servingSize: 8
     },
     {
-      id: 2,
-      image: "./images/banana-bread-photo.jpg",
+      id: 641836,
+      image: "./images/parma-chicken-photo.png",
       foodType: 'Chicken',
-      name: 'Balsamic Chicken'
+      name: 'Easy Baked Parmesan Chicken',
+      servingSize: 4
     },
     {
-      id: 3,
-      image: "./images/martha-stewart-photo.jpg",
-      foodType: 'Chicken',
-      name: 'Balsamic Chicken'
+      id: 645598,
+      image: "./images/turkey-risotto.png",
+      foodType: 'Turkey',
+      name: 'Creamy Risotto With Smoked Turkey Sausage',
+      servingSize: 6
     },
     {
-      id: 4,
-      image: "./images/apple-photo.jpg",
+      id: 646561,
+      image: "./images/chicken-corn-soup.png",
       foodType: 'Chicken',
-      name: 'Balsamic Chicken'
+      name: 'Hearty Chicken & Grilled Corn Soup',
+      servingSize: 4
+    },
+    {
+      id: 634496,
+      image: "./images/bbq-mac-cheese.png",
+      foodType: 'Side Dish',
+      name: 'BBQ Mac and Cheese',
+      servingSize: 4
+    },
+    {
+      id: 641836,
+      image: "./images/parma-chicken-photo.png",
+      foodType: 'Chicken',
+      name: 'Easy Baked Parmesan Chicken',
+      servingSize: 4
+    },
+    {
+      id: 645598,
+      image: "./images/turkey-risotto.png",
+      foodType: 'Turkey',
+      name: 'Creamy Risotto With Smoked Turkey Sausage',
+      servingSize: 6
+    },
+    {
+      id: 646561,
+      image: "./images/chicken-corn-soup.png",
+      foodType: 'Chicken',
+      name: 'Hearty Chicken & Grilled Corn Soup',
+      servingSize: 4
     },
   ]
+
+  // console.log(useSearchApi('waffle'));
+  // console.log(useRecipeId(634496))
 
   return (
     <>
